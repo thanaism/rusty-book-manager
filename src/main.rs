5188e692d1acf43bdca5e150bd79b6189c5ec2b5
thanaism <62784012+thanaism@users.noreply.github.com>
2024-11-03 21:59:01 +0900

    feat: Webサーバーの導入

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::net::{Ipv4Addr, SocketAddr};
+
+use anyhow::Result;
+use axum::{routing::get, Router};
+use axum::http::StatusCode;
+use tokio::net::TcpListener;
+
+pub async fn health_check() -> StatusCode {
+    StatusCode::OK
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = Router::new().route("/health", get(health_check));
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+    let listener = TcpListener::bind(addr).await?;
+    println!("Listening on {}", addr);
+    Ok(axum::serve(listener, app).await?)
+}
